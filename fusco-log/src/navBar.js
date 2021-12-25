b3f7adf9d929099ced4a019a3d06c0293c824b86
mattfusco7 <mattsterf@gmail.com>
2021-12-25 16:15:31 -0500

    small nav edits

--- fusco-log/src/navBar.js
+++ fusco-log/src/navBar.js
@@ -1,14 +1,20 @@
 import './styles/navBar.css';
 import { Link } from "react-router-dom";
+import thumbnail from "./images/thumbnail.png"
 
 function NavBar(props) {
 
     return (
-        <ul id="navJ">
-            <Link to="/">Home</Link>
-            <Link to="/settings">Profile</Link>
-            <div className="dot"></div>
-        </ul>
+        <>
+            <Link to="/">
+                <img src={thumbnail} alt="logo" id="thumbNav" />
+            </Link>
+            <ul id="navJ">
+                <Link to="/">Home</Link>
+                <Link to="/settings">Profile</Link>
+                <div className="dot"></div>
+            </ul>
+        </>
     );
 }
 
