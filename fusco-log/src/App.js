bd4313378da9884afbc781730caa96bcfbd47153
mattfusco7 <mattsterf@gmail.com>
2021-12-25 12:16:34 -0500

    login and password functionality complete

--- fusco-log/src/App.js
+++ fusco-log/src/App.js
@@ -5,6 +5,7 @@ import SignIn from './signInPage';
 import PrivateRoute from './privateRoute';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
+import ForgotPassword from './forgotPassword';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
 
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Switch>
       </AuthProvider>
     </Router>
--- fusco-log/src/forgotPassword.js
+++ fusco-log/src/forgotPassword.js
@@ -2,27 +2,27 @@ import './styles/signIn.css';
 import { useRef, useState } from 'react';
 import Header from './header';
 import { useAuth } from './contexts/AuthContext';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-function SignIn(props) {
+function ForgotPassword(props) {
     const EmailRef = useRef()
-    const PwdRef = useRef()
-    const { login } = useAuth()
+    const { resetPassword } = useAuth()
     const [error, setError] = useState('')
+    const [message, setMessage] = useState('')
     const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
 
     async function handleSubmit(e) {
         e.preventDefault()
 
         try {
+            setMessage('')
             setError('')
             setLoading(true)
-            await login(EmailRef.current.value, PwdRef.current.value)
-            navigate("/")
+            await resetPassword(EmailRef.current.value)
+            setMessage('Check your email inbox for further instructions.')
         } catch {
-            setError('Failed to sign in.')
+            setError('Failed to reset password.')
         }
 
         setLoading(false)
@@ -36,27 +36,26 @@ function SignIn(props) {
                 <div className="errorDiv">
                     {error}
                 </div>}
+            {message && <div className="errorDiv">
+                {message}
+            </div>}
             <form className="signForm" onSubmit={handleSubmit}>
                 <div className="formPair">
                     <label>Email: </label>
                     <input type="email" id="EMail" ref={EmailRef} required={true} />
                 </div>
-                <div className="formPair">
-                    <label>Password: </label>
-                    <input type="password" id="Pwd" ref={PwdRef} required={true} />
-                </div>
                 <button className="logIn-btn" type="submit" disabled={loading}>
-                    Log In
+                    Reset Password
                 </button>
                 <div className="bottomSignInLabel">
                     <Link className="link" to="/signup">Need an account? Sign up.</Link>
                 </div>
                 <div className="bottomSignInLabel forgot">
-                    <Link className="link" to="/forgot-password">Forgot password?</Link>
+                    <Link className="link" to="/signin">Log in.</Link>
                 </div>
             </form>
         </main>
     );
 }
 
-export default SignIn;
+export default ForgotPassword;
