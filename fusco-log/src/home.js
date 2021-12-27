4fe12ab4674fe50a91cd06aca56cf3b988302c92
mattfusco7 <mattsterf@gmail.com>
2021-12-26 20:00:20 -0500

    imported firestore

--- fusco-log/src/home.js
+++ fusco-log/src/home.js
@@ -1,7 +1,12 @@
 import './styles/home.css';
 import NavBar from './navBar';
+// import { db } from './firebase'
 
 function Home(props) {
+    // function handleClick() {
+    //     db.collection("testing").add({ "object": "please" })
+    // }
+
     return (
         <>
             <NavBar />
@@ -9,6 +14,7 @@ function Home(props) {
                 <h2>Home</h2>
                 <h3>Current Weekly Running Mileage: </h3>
                 <h3>Current Weekly Biking Time: </h3>
+                {/* <div className="btn" onClick={handleClick}>Test Firestore</div> */}
             </div>
         </>
     );
